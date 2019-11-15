36f6830218ced08a7fc9abe2d35a0006178956e4
Aditya Shrivastava <aditya.s@survey2connect.com>
2019-11-15 20:38:20 +0530

    Adding DB connection and validations on the server side

--- node-project/app.js
+++ node-project/app.js
@@ -16,4 +16,12 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(4300);
+//DB Connection:
+// <Issue> - Aditya local mongo db connection; <Fix> - Will later on fix this issue by adding a
+// seperate file for db connection variables and adding it to .gitignore
+
+mongoose.connect(
+  'mongodb://aditya:aditya123@127.0.0.1:27017/feed?authSource=admin'
+).then(result => {
+  app.listen(4300);
+}).catch(err => console.log(err));
--- node-project/controllers/feed.js
+++ node-project/controllers/feed.js
@@ -1,15 +1,51 @@
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First post', content: 'This is the first post!' }]
+    posts: [{
+      _id: '1'
+      title: 'First post',
+      content: 'This is the first post!',
+      imageUrl: 'images/happy-panda.png',
+      creator: {
+        name: 'Aditya'
+      },
+      createdAt: new Date()
+    }]
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Entered data does not meet the required validation',
+      errors: errors.array()
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/happy-panda.png',
+    creator: { name: 'Aditya' },
+  })
+  post.save()
+  .then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Post created successfully',
+      post: result
+    })
+  }
 
-  res.status(201).json({
-    message: 'Post created successfully',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  )
+  .catch(err => {
+    console.log(err)
   })
 }
