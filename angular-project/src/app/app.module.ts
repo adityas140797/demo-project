daee23b1fad33f28be82897c0465389772a04dbf
Aditya Shrivastava <aditya.s@survey2connect.com>
2019-11-16 15:24:15 +0530

    Improving the structure and project repo changed to angular-project

---
+++ angular-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signup/signup.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    SignUpComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
