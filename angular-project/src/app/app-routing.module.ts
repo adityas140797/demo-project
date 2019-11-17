3e0a02627f5ad42fb4a28f1bd0d9e3c24ad32adb
Aditya Shrivastava <aditya.s@survey2connect.com>
2019-11-18 00:46:27 +0530

    Adding bootstrap and jquery library and making card and form on modal on the front end side

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
- { path:'signup', component: SignUpComponent },
- { path:'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
